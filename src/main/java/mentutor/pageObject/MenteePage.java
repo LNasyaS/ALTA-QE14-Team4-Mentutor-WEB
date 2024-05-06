247d90b1517cee4343e46b0b8376f690e5e9bb7c
LNasya Syafitrie <lnasya.syafitrie@gmail.com>
2024-05-06 22:04:39 +0700

    dev initialize

---
+++ src/main/java/mentutor/pageObject/MenteePage.java
@@ -0,0 +1,12 @@
+package mentutor.pageObject;
+
+
+import mentutor.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class MenteePage extends BasePage {
+
+    public MenteePage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/main/java/mentutor/pageObject/MentorPage.java
@@ -0,0 +1,11 @@
+package mentutor.pageObject;
+
+import mentutor.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class MentorPage extends BasePage {
+
+    public MentorPage(WebDriver driver) {
+        super(driver);
+    }
+}
